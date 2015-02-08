e2e3b3aa0c434a1ab27e273020801342cc945041
Ramya <ramya@plivo.com>
2015-02-08 12:45:24 +0530

    Play XML

---
+++ voice/play_recorded.php
@@ -0,0 +1,30 @@
+<?php
+    require_once "./plivo.php";
+    require 'vendor/autoload.php';
+
+    $app->map('/speak', function() use ($app) {
+        
+        $res = new \Slim\Http\Response();
+        
+        // Generate a Play XML with the details of the audio file to play on the call.
+        $body = "https://s3.amazonaws.com/plivocloud/Trumpet.mp3";
+
+        $r = new Response(); 
+
+        // Add Play element
+        $r->addPlay($body);
+
+        $res->headers->set('Content-Type', 'text/xml');
+        $res->setBody($r->toXML());
+        $app->response = $res;
+
+    })->name('speak')->via('GET', 'POST');
+
+    $app->run();
+
+/*
+Sample Output
+<Response>
+    <Play>https://s3.amazonaws.com/plivocloud/Trumpet.mp3</Play>
+</Response>
+*/
