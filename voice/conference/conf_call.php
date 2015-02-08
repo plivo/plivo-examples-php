3c2c28b68cd62b23d0cbbf7548b345c8b315d911
Ramya <ramya@plivo.com>
2015-02-08 14:21:30 +0530

    conference calls

---
+++ voice/conference/conf_call.php
@@ -0,0 +1,35 @@
+<?php
+    require_once "./plivo.php";
+    
+    $auth_id = "Your AUTH_ID";
+    $auth_token = "Your AUTH_TOKEN";
+
+    $p = new RestAPI($auth_id, $auth_token);
+
+    $plivo_number = "1111111111";
+
+    $params = array(
+        'to' => '1111111111<2222222222', # The phon number to be called
+        'from' => $plivo_number, # The phone number to be used as the caller id
+        'answer_url' => "https://glacial-harbor-8656.herokuapp.com/testing.php/response/conference", # The URL invoked by Plivo when the outbound call is answered
+        'answer_method' => "GET" # The method used to call the answer_url
+    );
+    
+    $response = $p->make_call($params);
+    print_r ($response);
+    
+
+/* Sample Output
+( 
+    [status] => 201 
+    [response] => Array ( 
+        [api_id] => 8b32b934-af6e-11e4-b153-22000abcaa64 
+        [message] => calls fired 
+        [request_uuid] => Array ( 
+            [0] => d1187266-b5e8-4a1d-bd8d-b0a4395f08f6 
+            [1] => 0c88ed10-fde4-4e6e-8c7c-08ebeee5e856 
+        ) 
+    ) 
+)
+*/
+
---
+++ voice/conference/conference_xml.php
@@ -0,0 +1,89 @@
+<?php
+    require_once "./plivo.php";
+    require 'vendor/autoload.php';
+
+    $app->map('/response/conference', function() use ($app) {
+        $res = new \Slim\Http\Response();
+
+        $r = new Response();
+
+        $body = "You will now be placed into a demo conference. This is brought to you by Plivo. To know more visit us at plivo.com";
+        $r->addSpeak($body);
+
+        $params = array(
+                'enterSound' => "beep:2", # Used to play a sound when a member enters the conference
+                'record' => "true", # Option to record the call
+                'action' => "https://glacial-harbor-8656.herokuapp.com/testing.php/response/conf_action/", # URL to which the API can send back parameters
+                'method' => "GET", # method to invoke the action Url
+                'record' => "true", # Option to record the call 
+                'callbackUrl' => "hhttps://glacial-harbor-8656.herokuapp.com/testing.php/response/conf_callback/", # If specified, information is sent back to this URL
+                'callbackMethod' => "GET" # Method used to notify callbackUrl
+                # For moderated conference
+                'startConferenceOnEnter' => "true", # When a member joins the conference with this attribute set to true, the conference is started.
+                                       # If a member joins a conference that has not yet started, with this attribute value set to false, 
+                                       # the member is muted and hears background music until another member joins the conference
+                'endConferenceOnExit' => "true" # If a member with this attribute set to true leaves the conference, the conference ends and all 
+                                   # other members are automatically removed from the conference. 
+            );
+        
+        $conference_nae = "demo";
+        $r->addConference($conference_name, $params);
+
+        $res->headers->set('Content-Type', 'text/xml');
+        $res->setBody($r->toXML());
+        $app->response = $res;
+
+    })->name('conference')->via('GET', 'POST');
+
+    $app->map('/response/conf_action', function() use ($app) {
+
+        $conf_name = $_REQUEST['ConferenceName'];
+        $conf_uuid = $_REQUEST['ConferenceUUID'];
+        $conf_mem_id = $_REQUEST['ConferenceMemberID'];
+        $record_url = $_REQUEST['RecordUrl'];
+        $record_id = $_REQUEST['RecordingID'];
+
+        error_log("Conference Name : $conf_name, Conference UUID : conf_uuid, Conference Member ID : conf_mem_id, Record URL : $record_url, Record ID : $record_id");
+        echo "Conference Name : $conf_name, Conference UUID : conf_uuid, Conference Member ID : conf_mem_id, Record URL : $record_url, Record ID : $record_id";
+
+    })->name('conf_action')->via('GET', 'POST');
+  
+    $app->map('/response/conf_callback', function() use ($app) {
+
+        $conf_action = $_REQUEST['ConferenceAction'];
+        $conf_name = $_REQUEST['ConferenceName'];
+        $conf_uuid = $_REQUEST['ConferenceUUID'];
+        $conf_mem_id = $_REQUEST['ConferenceMemberID'];
+        $call_uuid = $_REQUEST['CallUUID'];
+        $record_url = $_REQUEST['RecordUrl'];
+        $record_id = $_REQUEST['RecordingID'];
+
+        error_log("Conference Action : $conf_action, Conference Name : $conf_name, Conference UUID : conf_uuid, 
+            Conference Member ID : conf_mem_id, Call UUID : $call_uuid, Record URL : $record_url, Record ID : $record_id");
+        echo "Conference Action : $conf_action, Conference Name : $conf_name, Conference UUID : conf_uuid, 
+            Conference Member ID : conf_mem_id, Call UUID : $call_uuid, Record URL : $record_url, Record ID : $record_id";
+
+    })->name('conf_callback')->via('GET', 'POST');      
+
+    $app->run();
+
+/*
+Sample Output
+<Response>
+    <Speak>
+        You will now be placed into a demo conference. This is brought to you by Plivo. To know more visit us at plivo.com
+    </Speak>
+    <Conference enterSound="beep:2" record="true" action="https://glacial-harbor-8656.herokuapp.com/testing.php/response/conf_action" 
+        method="GET" callbackUrl="https://glacial-harbor-8656.herokuapp.com/testing.php/response/conf_callback" 
+        callbackMethod="GET" startConferenceOnEnter="true" endConferenceOnExit="true">demo</Conference>
+</Response>
+
+Conference Name : demo, Conference UUID : 97cb91e8-af6e-11e4-8e67-377ffe01233f, Conference Member ID : 1129, 
+Record URL : http://s3.amazonaws.com/recordings_2013/9792115c-vvvv-11e4-a664-0026b945b52b.mp3, Record ID : 9792115c-af6e-11e4-a664-0026b945b52b
+
+Conference Action : enter, Conference Name : demo, Conference UUID : 97cb91e8-af6e-11e4-8e67-377ffe01233f, 
+Conference Member ID : 1130, Call UUID : 8bd540aa-af6e-11e4-87a9-377ffe01233f, 
+Record URL : http://s3.amazonaws.com/recordings_2013/9792115c-wwww-11w4-a664-0026b945b52b.mp3, Record ID : 9792115c-af6e-11e4-a664-0026b945b52b
+
+
+*/
