edc519842cafc87b9059e0060da999d915a75847
Ramya <ramya@plivo.com>
2015-02-24 16:37:44 +0530

    Records the legs of a call separately

---
+++ voice/record/legs_rec.php
@@ -0,0 +1,138 @@
+<?php
+    require_once "./plivo.php";
+    require 'vendor/autoload.php';
+
+    $app = new \Slim\Slim();
+
+    # A call is made to the plivo number. 
+    # The answer_url returns and XML that starts recording the session and then dials to another number.
+    # When the user pick up, the B Leg record starts and a music is played.
+
+    # The action URL of the Record tag will return the Session recording details
+
+    $app->map('/answer_incoming', function() use ($app) {
+        $res = new \Slim\Http\Response();
+        $r = new Response(); 
+
+        $record_params = array(
+            'action' => 'https://glacial-harbor-8656.herokuapp.com/testing.php/record_action', # Submit the result of the record to this URL. 
+            'method' => 'GET', # Submit to action url using GET or POST
+            'redirect' => 'false', # If false, don't redirect to action url, only request the url and continue to next element.
+            'recordSession' => 'true' # Record current call session in background 
+        );
+
+        $r->addRecord($record_params);
+
+        $wait_params = array(
+            'length' => '5' # Time to wait in seconds
+          );
+
+        $r->addWait($wait_params);
+        $r->addSpeak("Connecting your call!");
+
+        $dial_params = array(
+            'callbackUrl' => 'https://glacial-harbor-8656.herokuapp.com/testing.php/dial_outbound', # URL that is notified by Plivo when one of the following events occur :    called party is bridged with caller, called party hangs up, caller has pressed any digit
+            'callbackMethod' => 'GET' # Method used to notify callbackUrl.
+        );
+
+        $number = "1111111111";
+        $d = $r->addDial($dial_params);
+        $d->addNumber($number);
+
+        $res->headers->set('Content-Type', 'text/xml');
+        $res->setBody($r->toXML());
+        print($r->toXML());
+        $app->response = $res;
+
+    })->name('answer_incoming')->via('GET','POST');
+
+    # The Callback URL of Dial will make a request to the Record API which will record only the B Leg
+    # Play API is invoked which will play a music only on the B Leg.
+
+    $app->map('/dial_outbound', function() use ($app) {
+        $event = $_REQUEST['Event'];
+        $call_uuid = $_REQUEST['DialBLegUUID'];    
+
+        print("Event : $event");
+        print("Call UUID : $call_uuid");
+
+        if($event == "DialAnswer")
+        {
+            $auth_id = "Your AUTH_ID";
+            $auth_token = "Your AUTH_TOKEN";
+
+            $p = new RestAPI($auth_id, $auth_token);
+            $record_params = array(
+                'call_uuid' => $call_uuid, # ID of the call 
+                'callback_url' => 'https://glacial-harbor-8656.herokuapp.com/testing.php/recording_callback', # The URL invoked by the API when the recording ends.
+                'callback_method' => 'GET' # The method which is used to invoke the callback_url URL. Defaults to POST.
+            );
+
+            $resp = $p->record($record_params);
+            print("API ID : {$resp['response']['api_id']}");
+            print("Message : {$resp['response']['message']}");
+
+            $play_params = array(
+                'call_uuid' => $call_uuid, # ID of the call
+                'urls' => "https://s3.amazonaws.com/plivocloud/Trumpet.mp3" # A single URL or a list of comma separated URLs pointing to an mp3 or wav file.     
+            );
+
+            $pl = new RestAPI($auth_id, $auth_token);
+            $response = $pl->play($play_params););
+            print("API ID : {$response['response']['api_id']}");
+            print("Message : {$response['response']['message']}");
+        }
+        else
+        {
+            print("Invalid");
+        }
+
+    })->name('dial_outbound')->via('GET','POST');
+
+    # The Callback URL of record api will return the B Leg record details.
+    $app->map('/recording_callback', function() use ($app) {
+
+        $record_url = $_REQUEST['record_url'];
+        $record_duration = $_REQUEST['recording_duration'];
+        $record_id = $_REQUEST['recording_id'];
+        print("Record URL : $record_url");
+        print("Recording Duration : $record_duration");
+        print("Recording ID : $record_id");
+
+    })->name('recording_callback')->via('GET','POST');
+
+    $app->run();
+
+/*
+Sample Output
+<Response>
+    <Record action="https://glacial-harbor-8656.herokuapp.com/testing.php/record_action" method="GET" redirect="false" recordSession="true"/>
+    <Wait length="5"/>
+    <Speak>Connecting your call!</Speak>
+    <Dial callbackUrl="https://glacial-harbor-8656.herokuapp.com/testing.php/dial_outbound" callbackMethod="GET">
+        <Number>1111111111</Number>
+    </Dial>
+</Response>
+
+Call UUID : fead401c-bc13-11e4-be69-b7d6ae170e26
+Event : DialAnswer
+
+Output of the Record API request
+API ID : 050d62c0-bc14-11e4-b423-22000ac8a2f8
+Message : async api spawned
+
+Output of the Play API request
+API ID : 05436366-bc14-11e4-8ccf-22000afb14f7
+Message : play started
+
+Output of Record API Callback URL
+Record URL : http://s3.amazonaws.com/recordings_2013/11112222-4444-11e4-a4c8-782bcb5bb8af.mp3
+Recording Duration : 22 
+Recording ID : 693e61fd-8150-4091-a0f8-561d4a434288 
+
+Output of Record XML Action URL
+Record URL : http://s3.amazonaws.com/recordings_2013/55556666-7777-11e4-a4c8-782bcb5bb8af.mp3 
+Recording Duration : 27 
+Recording ID : daddbf04-9585-11e4-a4c8-782bcb5bb8af 
+
+*/
