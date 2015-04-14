c410d01e1095d4fb14e63439e10dc732db985bdc
Ramya <ramya@plivo.com>
2015-04-14 18:33:48 +0530

    removed Framework and other dependencies

--- voice/call_api/blacklist.php
+++ voice/call_api/blacklist.php
@@ -1,14 +1,9 @@
 <?php
     require_once "./plivo.php";
-    require 'vendor/autoload.php';
-
-    $app = new \Slim\Slim();
-
-    $app->map('/reject_caller', function() use ($app) {
 
     $from_number = $_REQUEST['From'];
     $caller = array('1111111111', '2222222222', '3333333333');
-    $res = new \Slim\Http\Response();
+
     $r = new Response();
     if (in_array($from_number, $caller))
     {
@@ -23,14 +18,8 @@
         $r->addSpeak($body);
     }
 
-    $res->headers->set('Content-Type', 'text/xml');
-    $res->setBody($r->toXML());
-    error_log($r->toXML());
-    $app->response = $res;
-
-    })->name('reject_caller')->via('GET','POST');
-
-    $app->run();
+    Header('Content-type: text/xml');
+    echo($r->toXML());
 
 /*
 Sample output when From number is in blacklist
