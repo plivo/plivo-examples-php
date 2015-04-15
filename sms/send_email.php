003f864d2cae3b2065bafefb1a0cdf6fbf11b11f
Ramya <ramya@plivo.com>
2015-04-15 13:05:51 +0530

    removed Framework and other dependencies

--- sms/send_email.php
+++ sms/send_email.php
@@ -1,59 +1,52 @@
 <?php
     // Pear Mail Library
     require_once "Mail.php";
-
-    use GuzzleHttp\Client;
-    require 'vendor/autoload.php';
-    $app = new \Slim\Slim();
     
-    $app->map('/receive_sms', function () use ($app) {
-        // Sender's phone numer
-        $from_number = $_REQUEST["From"];
+    // Sender's phone numer
+    $from_number = $_REQUEST["From"];
 
-        // Receiver's phone number - Plivo number
-        $to_number = $_REQUEST["To"];
+    // Receiver's phone number - Plivo number
+    $to_number = $_REQUEST["To"];
 
-        // The SMS text message which was received
-        $text = $_REQUEST["Text"];
+    // The SMS text message which was received
+    $text = $_REQUEST["Text"];
 
-        // Output the text which was received, you could
-        // also store the text in a database.
-        echo("Message received from $from_number : $text");
-        to_email($text);
-    })->name('receive_sms')->via('GET','POST');
+    // Output the text which was received, you could
+    // also store the text in a database.
+    echo("Message received from $from_number : $text");
+    to_email($text);
 
-    function to_email($text){
+function to_email($text){
 
-        $from = 'From mail address';
-        $to = 'To mail address';
-        $subject = 'Testing mail'; // Subject of the mail
-        $body = $text;
+    $from = 'From mail address';
+    $to = 'To mail address';
+    $subject = 'Mail Subject';
+    $body = $text;
 
-        $headers = array(
-            'From' => $from,
-            'To' => $to,
-            'Subject' => $subject
-        );
+    $headers = array(
+        'From' => $from,
+        'To' => $to,
+        'Subject' => $subject
+    );
 
-        $smtp = Mail::factory('smtp', array(
-                'host' => 'ssl://smtp.gmail.com',
-                'port' => '465',
-                'auth' => true,
-                'username' => 'Your username',
-                'password' => 'Your password'
-            ));
+    $smtp = Mail::factory('smtp', array(
+            'host' => 'ssl://smtp.gmail.com',
+            'port' => '465',
+            'auth' => true,
+            'username' => 'Your mail address',
+            'password' => 'Your Password'
+        ));
 
-        // Send mail
-        $mail = $smtp->send($to, $headers, $body);
+    // Send mail
+    $mail = $smtp->send($to, $headers, $body);
 
-        if (PEAR::isError($mail)) {
-            echo('<p>' . $mail->getMessage() . '</p>');
-        } else {
-            echo('<p>Message successfully sent!</p>');
-        }
+    if (PEAR::isError($mail)) {
+        echo('<p>' . $mail->getMessage() . '</p>');
+    } else {
+        echo('<p>Message successfully sent!</p>');
     }
+}
 
-    $app->run();
 
 ?>
 
