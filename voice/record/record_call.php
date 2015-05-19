ce9fa8fcc0b3be438b0a187996b058ae91e5df3d
Ramya <ramya@plivo.com>
2015-05-19 11:49:47 +0530

    removed framework

--- voice/record/record_call.php
+++ voice/record/record_call.php
@@ -1,55 +1,48 @@
 <?php
     require_once "./plivo.php";
-    require 'vendor/autoload.php';
 
-    $app = new \Slim\Slim();
 
     # Generate a Record XML
-    $app->map('/record', function() use ($app) {
+    $r = new Response(); 
 
-        $res = new \Slim\Http\Response();
-        $r = new Response(); 
+    $record_params = array(
+        'action'=> 'https://example.com/record_action', # Submit the result of the record to this URL
+        'method' => 'GET', # HTTP method to submit the action URL
+        'callbackUrl' => 'https://example.com/recording_callback', # If set, this URL is fired in background when the recorded file is ready to be used.
+        'callbackMethod' => 'GET' # Method used to notify the callbackUrl.
+    );
 
-        $record_params = array(
-            'action'=> 'https://glacial-harbor-8656.herokuapp.com/testing.php/record_action', # Submit the result of the record to this URL
-            'method' => 'GET', # HTTP method to submit the action URL
-            'callbackUrl' => 'https://glacial-harbor-8656.herokuapp.com/testing.php/recording_callback', # If set, this URL is fired in background when the recorded file is ready to be used.
-            'callbackMethod' => 'GET' # Method used to notify the callbackUrl.
-        );
+    $r->addRecord($record_params);
+    Header('Content-type: text/xml');
+    echo($r->toXML());  
+?>
 
-        $r->addRecord($record_params);
-        $res->headers->set('Content-Type', 'text/xml');
-        $res->setBody($r->toXML());
-        print($r->toXML());
-        $app->response = $res;      
+<!--record_action.php-->
 
-    })->name('record')->via('GET','POST');
-
+<?php
     # Action URL Example
-    $app->map('/record_action', function() use ($app) {
 
-        $record_url = $_REQUEST['RecordUrl'];
-        $record_duration = $_REQUEST['RecordingDuration'];
-        $record_id = $_REQUEST['RecordingID'];
-        print("Record URL : $record_url");
-        print("Recording Duration : $record_duration");
-        print("Recording ID : $record_id");
+    $record_url = $_REQUEST['RecordUrl'];
+    $record_duration = $_REQUEST['RecordingDuration'];
+    $record_id = $_REQUEST['RecordingID'];
+    print("Record URL : $record_url");
+    print("Recording Duration : $record_duration");
+    print("Recording ID : $record_id");
 
-    })->name('record_action')->via('GET','POST');
+?>
+
+<!--recording_callback.php-->
+
+<?php
 
     # Callback URL Example
-    $app->map('/recording_callback', function() use ($app) {
+    $record_url = $_REQUEST['record_url'];
+    $record_duration = $_REQUEST['recording_duration'];
+    $record_id = $_REQUEST['recording_id'];
+    print("Record URL : $record_url");
+    print("Recording Duration : $record_duration");
+    print("Recording ID : $record_id");
 
-        $record_url = $_REQUEST['record_url'];
-        $record_duration = $_REQUEST['recording_duration'];
-        $record_id = $_REQUEST['recording_id'];
-        print("Record URL : $record_url");
-        print("Recording Duration : $record_duration");
-        print("Recording ID : $record_id");
-
-    })->name('recording_callback')->via('GET','POST');
-
-    $app->run();
 
 /*
 Sample Output
